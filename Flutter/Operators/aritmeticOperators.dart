b16566fea70efadd3a9cf9bf73f206a4d83a8cc3
NorderBlack <carlossagvinos@gmail.com>
2023-01-16 18:39:40 +0100

    Adding operators folder

---
+++ Flutter/Operators/aritmeticOperators.dart
@@ -0,0 +1,13 @@
+void main() {
+  int suma = 5 + 6 + 7 + 8;
+  double resta = 5 - 6 - 7.5 - 8.4;
+  int multiplication = 5 * 8;
+  double division = 7 / 25;
+  double resto = 7 % 7; //resto de la division
+
+  print(suma);
+  print(resta);
+  print(multiplication);
+  print(division);
+  print(modulo);
+}
