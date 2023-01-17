ddd875ed6ac6623178c7ddd98c21df30956a4b1a
NorderBlack <carlossagvinos@gmail.com>
2023-01-17 18:27:50 +0100

    adding

---
+++ Flutter/Control Structure/switch.dart
@@ -0,0 +1,14 @@
+void main() {
+  int selectedOption = 1;
+
+  switch (selectedOption) {
+    case 1:
+      print("Option 1");
+      break;
+    case 2:
+      print("Option 2");
+      break;
+    default:
+      print("Invalid option");
+  }
+}
